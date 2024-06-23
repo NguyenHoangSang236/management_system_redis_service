39a2bf4011bf7fa1b9f410f486d57a92265a82ee
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-06-23 16:46:20 +0700

    add eureka client

--- src/main/java/com/management_system/redis_service/RedisServiceApplication.java
+++ src/main/java/com/management_system/redis_service/RedisServiceApplication.java
@@ -3,6 +3,7 @@ package com.management_system.redis_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(
@@ -22,6 +23,7 @@ import org.springframework.context.annotation.ComponentScan;
 		"com.management_system.redis_service.services",
 		"com.management_system.utilities",
 })
+@EnableDiscoveryClient
 public class RedisServiceApplication {
 
 	public static void main(String[] args) {

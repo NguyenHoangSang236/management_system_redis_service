d948ca7799eda0b9ba7883ff92a4d25cdd714e20
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-06-27 22:02:37 +0700

    update build script

--- build_redis_service.sh
+++ build_redis_service.sh
@@ -1,4 +1,4 @@
 ./mvnw clean install package spring-boot:repackage
-sudo docker rm -f redis_service_container
-sudo docker build --tag=redis_service_container .
-sudo docker run -p 8079:8079 --name redis_service_container redis_service_container
+sudo docker rm -f redis-service-container
+sudo docker build -t redis-service-container .
+sudo docker run -p 8079:8079 --name redis-service-container redis-service-container

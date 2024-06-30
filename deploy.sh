18b87613d49b973ae11202af819d4036d116c4ca
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-06-30 18:00:17 +0700

    edit deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ split -b 21M target/FoolishStoreProject-0.0.1-SNAPSHOT.jar splited-app-
 git add .
 git commit -m 'deploy'
 git push origin deployment
-ssh root@103.200.20.153 'cd FoolishStoreProject && git pull origin build && cat splited-app-* > target/redis_service-0.0.1-SNAPSHOT.jar && docker rm -f redis-service-container && docker compose up -d --build && rm -r splited-app-*'
+ssh root@103.200.20.153 'cd management_system_redis_service && rm -f target/redis_service-0.0.1-SNAPSHOT.jar && git pull origin deployment && cat splited-app-* > target/redis_service-0.0.1-SNAPSHOT.jar && docker rm -f redis-service-container && docker compose up -d --build && rm -r splited-app-*'

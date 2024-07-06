7eb1aa6ba2f300b5c662a83ef9add93cdbedc74f
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-07-06 18:06:34 +0700

    deploy

--- build_redis_service.sh
+++ build_redis_service.sh
@@ -8,7 +8,7 @@ ssh root@103.200.20.153 << 'EOF'
 cd management_system_redis_service
 git pull origin deployment
 rm -r target/redis_service-0.0.1-SNAPSHOT.jar
-cat splited-app-* > rm -r
+cat splited-app-* > target/redis_service-0.0.1-SNAPSHOT.jar
 docker rm -f redis-service-container
 docker build -t redis-service-container .
 docker run -p 8079:8079 --name redis-service-container redis-service-container

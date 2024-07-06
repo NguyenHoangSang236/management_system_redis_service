875f28967d0d2d8b9ec95c40967be23b8ff67519
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-07-06 15:52:29 +0700

    update build script

--- build_redis_service.sh
+++ build_redis_service.sh
@@ -1,4 +1,15 @@
 ./mvnw clean install package spring-boot:repackage
-sudo docker rm -f redis-service-container
-sudo docker build -t redis-service-container .
-sudo docker run -p 8079:8079 --name redis-service-container redis-service-container
+rm splited-app-* | true
+split -b 21M target/redis_service-0.0.1-SNAPSHOT.jar splited-app-
+git add .
+git commit -m 'deploy'
+git push origin deployment
+ssh root@103.200.20.153 << 'EOF'
+cd management_system_redis_service
+git pull origin deployment
+cat splited-app-* > target/redis_service-0.0.1-SNAPSHOT.jar
+docker rm -f redis-service-container
+docker build -t redis-service-container .
+docker run -p 8079:8079 --name redis-service-container redis-service-container
+rm -r splited-app-*
+EOF

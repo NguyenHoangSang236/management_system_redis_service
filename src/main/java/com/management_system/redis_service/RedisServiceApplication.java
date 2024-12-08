ed16fb46443c9f4d96e1e073e442a599ab71a2ec
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-12-08 18:39:41 +0700

    update configs

--- src/main/java/com/management_system/redis_service/RedisServiceApplication.java
+++ src/main/java/com/management_system/redis_service/RedisServiceApplication.java
@@ -1,10 +1,14 @@
 package com.management_system.redis_service;
 
+import com.management_system.utilities.utils.InitiateConfigUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication(
         exclude = {SecurityAutoConfiguration.class},
@@ -25,8 +29,15 @@ import org.springframework.context.annotation.ComponentScan;
 })
 //@EnableDiscoveryClient
 public class RedisServiceApplication {
+    @Autowired
+    InitiateConfigUtils initiateConfigUtils;
+
     public static void main(String[] args) {
         SpringApplication.run(RedisServiceApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void initSslConfig() {
+        initiateConfigUtils.initSslConfig();
+    }
 }
